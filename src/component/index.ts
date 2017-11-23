c0956e30dc16ebb3edc558530c27defd86301802
sis0k0 <stanimira.vlaeva@gmail.com>
2017-11-23 15:17:32 +0200

    initial

---
+++ src/component/index.ts
@@ -0,0 +1,18 @@
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+  externalSchematic,
+} from '@angular-devkit/schematics';
+
+import { Schema as ComponentOptions } from './schema';
+
+export default function(options: ComponentOptions): Rule {
+    return chain([
+        externalSchematic('@schematics/angular', 'component', options),
+        (tree: Tree, _context: SchematicContext) => {
+            return tree;
+        }
+    ])
+}
