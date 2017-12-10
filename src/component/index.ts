ab7193bb47249a10e32df72c22eb3e25bbdf6e8e
sis0k0 <stanimira.vlaeva@gmail.com>
2017-12-10 16:22:45 -0500

    refactor(component): create templates with platform-specific extensions

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,5 +1,6 @@
 import {
   Rule,
+  SchematicContext,
   SchematicsException,
   Tree,
   apply,
@@ -19,26 +20,65 @@ import { dasherize } from '@schematics/angular/strings';
 import {
   addSymbolToComponentMetadata,
   getSourceFile,
+  ns,
+  web,
+  getExtensions,
 } from "../utils";
 import { Schema as ComponentOptions } from './schema';
 import { Path, normalize } from '@angular-devkit/core';
 
+let extensions;
 export default function (options: ComponentOptions): Rule {
   let { name, sourceDir, path, flat } = options;
   name = dasherize(name);
-  const componentPath = normalize(
+  const dest = normalize(
     `/${sourceDir}/${path}/`
     + (flat ? '' : name + '/')
-    + name + '.component.ts'
   );
+  const componentPath = normalize(`${dest}/${name}.component.ts`);
+  const templatePath = normalize(`${dest}/${name}.component.html`);
 
   return chain([
     externalSchematic('@schematics/angular', 'component', options),
-    filter((path: Path) => !path.match(/\.html$/)),
-    insertModuleId(componentPath),
-    addFiles(options),
+    validateOptions(options),
+
+    (tree: Tree) => {
+      extensions = getExtensions(tree);
+      return tree;
+    },
+
+    (tree: Tree, context: SchematicContext) => web(tree, options) ?
+      templateToWeb(templatePath)(tree) :
+      filter((path: Path) => !path.match(templatePath))(tree, context),
+
+    (tree: Tree) => ns(tree, options) ?
+      insertModuleId(componentPath)(tree) :
+      tree,
+
+    (tree: Tree, context: SchematicContext) => ns(tree, options) ?
+      addFiles(options)(tree, context) :
+      tree,
   ]);
-}
+};
+
+const validateOptions = (options: ComponentOptions) =>
+  (tree: Tree) => {
+    const isWeb = web(tree, options);
+    const isNs = ns(tree, options);
+    if (!isWeb && !isNs) {
+      throw new SchematicsException('You need to specify project type!');
+    }
+
+    if (isWeb && isNs && options.inlineTemplate) {
+      throw new SchematicsException('You cannot use the --inlineTemplate option for web+ns component!');
+    }
+  };
+
+const templateToWeb = (path: string) =>
+  (tree: Tree) => {
+    const webName = path.replace(".html", `${extensions.web}.html`);
+    tree.rename(path, webName);
+  };
 
 const insertModuleId = (component: string) =>
   (tree: Tree) => {
