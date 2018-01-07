81693a204a439aca83499a4996390879c40d136a
sis0k0 <stanimira.vlaeva@gmail.com>
2018-01-07 06:11:12 -0500

    feat: add application schematic

---
+++ src/application/index.ts
@@ -0,0 +1,24 @@
+import {
+  apply,
+  url,
+  move,
+  template,
+  branchAndMerge,
+  mergeWith,
+  TemplateOptions,
+} from '@angular-devkit/schematics';
+import * as stringUtils from '@schematics/angular/strings';
+
+export default function (options: any) {
+  console.log(options);
+  return branchAndMerge(mergeWith(
+    apply(url('./files'), [
+      template(<TemplateOptions>{
+        utils: stringUtils,
+        sourcedir: options.sourceDir || 'app',
+        ...options as any,
+      }),
+      move(options.name || '.'),
+    ])
+  ))
+}
