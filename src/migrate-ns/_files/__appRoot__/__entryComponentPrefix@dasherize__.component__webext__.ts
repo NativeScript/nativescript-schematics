5e5161c4090f4b0298651ab55a3c9b172a236fa8
Sebastian Witalec <sebawita@users.noreply.github.com>
2018-03-20 17:41:01 +0200

    feat: add migration schematic from {N} to web+{N} shared project (#3)

---
+++ src/migrate-ns/_files/__appRoot__/__entryComponentPrefix@dasherize__.component__webext__.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '<%= indexAppRootTag %>',
+  templateUrl: './app.component.html'
+})
+export class <%= entryComponentName %> {
+  title = 'app';
+}
