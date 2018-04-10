8c0de3038c82299c00d719d66c3aa97e6510f07c
sebawita <sebawita@gmail.com>
2018-04-11 00:30:53 +0100

    Adding __platform__ as a workaround to .tns

---
+++ src/add-ns/_ns-files/__sourceDir__/app/__entryComponentPrefix@dasherize__.component__platform__.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '<%= indexAppRootTag %>',
+  moduleId: module.id,
+  templateUrl: '<%= entryComponentImportPath %>.html',
+})
+
+export class <%= entryComponentName %> { }
