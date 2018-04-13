4384a04b0205fbf9ab6de0b20744d127f72cd863
sebawita <sebawita@gmail.com>
2018-04-13 11:26:03 +0300

    Rename sourcedir to sourceDir

---
+++ src/add-ns/_ns-files/__sourceDir__/app/app.routing__nsext__.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/players', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [NativeScriptRouterModule.forRoot(routes)],
+  exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
