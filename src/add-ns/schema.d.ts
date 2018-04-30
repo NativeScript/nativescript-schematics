420c9a11c9da1ece2837302e66a1c791cdcd02a5
sebawita <sebawita@gmail.com>
2018-04-30 19:55:18 +0100

    remove unused property

--- src/add-ns/schema.d.ts
+++ src/add-ns/schema.d.ts
@@ -7,8 +7,4 @@ export interface Schema {
    * Allows specification of the used extension for web
    */
   webExtension: string;
-  /**
-   * Used to specify if using ng cli v1, true by default
-   */
-  cliV1: boolean;
 }
