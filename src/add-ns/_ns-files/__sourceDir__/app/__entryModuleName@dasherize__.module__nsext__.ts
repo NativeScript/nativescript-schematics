9b7bf258643227706b431adfad29103f769d91a8
sebawita <sebawita@gmail.com>
2018-07-04 17:47:31 +0300

    Update app routing module file name

--- src/add-ns/_ns-files/__sourceDir__/app/__entryModuleName@dasherize__.module__nsext__.ts
+++ src/add-ns/_ns-files/__sourceDir__/app/__entryModuleName@dasherize__.module__nsext__.ts
@@ -1,6 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-import { AppRoutingModule } from './app.routing<%= nsext %>';
+import { AppRoutingModule } from './app-routing.module<%= nsext %>';
 import { <%= entryComponentClassName %> } from '<%= entryComponentImportPath %>';
 
 import { BarcelonaModule } from './barcelona/barcelona.module';
