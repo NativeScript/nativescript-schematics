eb8b253bed2fbcde8079176e2df15181338d054e
sebawita <sebawita@gmail.com>
2018-07-18 18:22:50 +0100

    Add --style=scss option to the ng-new shared application schematic

--- src/ng-new/shared/_files/__sourcedir__/app/app.component.ts
+++ src/ng-new/shared/_files/__sourcedir__/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: '<%= prefix %>-root',
   moduleId: module.id,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.<%= style %>']
 })
 export class AppComponent {
   title = '<%= name %>';
--- src/ng-new/shared/schema.d.ts
+++ src/ng-new/shared/schema.d.ts
@@ -11,4 +11,12 @@ export interface Schema {
      * The prefix to apply to generated selectors.
      */
     prefix: string;
+    /**
+     * The file extension to be used for style files.
+     */
+    style: string;
+    /**
+     * Specifies whether the {N} theme for styling should be included.
+     */
+    theme: boolean;
 }
