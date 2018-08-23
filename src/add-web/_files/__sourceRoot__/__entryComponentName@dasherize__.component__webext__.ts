a699580d1a8c0736f3059ea6f0ab9be459c95787
sis0k0 <stanimira.vlaeva@gmail.com>
2018-08-23 19:35:11 +0300

    refactor: remove outdated add-web schematic

--- src/add-web/_files/__sourceRoot__/__entryComponentName@dasherize__.component__webext__.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: '<%= indexAppRootTag %>',
-  templateUrl: '<%= entryComponentImportPath %>.html'
-})
-export class <%= entryComponentClassName %> {
-  title = 'app';
-}
