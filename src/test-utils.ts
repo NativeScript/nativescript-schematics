06e4a7f760b6c06f54679f220423f662504b0728
sis0k0 <stanimira.vlaeva@gmail.com>
2019-05-06 09:00:58 -0700

    refactor: set up the test tree with memory FS
    This allows us to have a base instead of actions in the test tree. We
    need this in order to test the behaviour of the schematic when applied
    to tree without create actions.

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,3 +1,12 @@
+import { UnitTestTree } from "@angular-devkit/schematics/testing";
+import { HostTree } from "@angular-devkit/schematics";
+import { virtualFs, Path } from "@angular-devkit/core";
+
+export interface VirtualFile {
+  path: string;
+  content: string;
+}
+
 export const isInModuleMetadata = (
     moduleName: string,
     property: string,
@@ -56,3 +65,42 @@ const keyValueString = (
     property: string,
     value: string,
 ) => `${property}: ${value}`;
+
+export function setupTestTreeWithBase(files: VirtualFile[]): UnitTestTree {
+  const memoryFs = setupInMemoryBase(files);
+  const host = new HostTree(memoryFs);
+  const tree = new UnitTestTree(host);
+
+  return tree;
+}
+
+function setupInMemoryBase(files: VirtualFile[]): virtualFs.SimpleMemoryHost {
+  const memoryFs = new virtualFs.SimpleMemoryHost();
+  files.forEach(file => {
+    const path = file.path as Path;
+    // The write method of memoryFs expects an ArrayBuffer.
+    // However, when the read method is used to fetch the file from the FS
+    // the returned value is not converted properly to string.
+    // This is why we're using node Buffer to write the file in the memory FS.
+    // const content = stringToArrayBuffer(file.content);
+    const content = Buffer.from(file.content);
+
+    (<any>memoryFs).write(path, content).subscribe();
+  });
+
+  return memoryFs;
+}
+
+function stringToArrayBuffer(text: string): ArrayBuffer {
+  const stringLength = text.length;
+
+  const byteLength = stringLength * 2; // 2 bytes for each character
+  const arrayBuffer = new ArrayBuffer(byteLength);
+  const bufferView = new Uint16Array(arrayBuffer);
+
+  for (let i = 0; i < stringLength; i += 1) {
+    bufferView[i] = text.charCodeAt(i);
+  }
+
+  return arrayBuffer;
+}
